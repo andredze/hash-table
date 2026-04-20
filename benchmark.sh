9872532b9e20e411b32c380b63609e0001e586a8
andredze <an.kiselev.2008@gmail.com>
2026-04-20 18:57:52 +0300

    benchmarking with hyperfine script

---
+++ benchmark.sh
@@ -0,0 +1,23 @@
+# isolate core 2 from regular tasks
+isolcpus=2
+
+# isolate core 2 from clock interrupts
+nohz_full=2
+
+# limit processor's frequency
+sudo cpupower frequency-set --max 2.80GHz
+sudo cpupower frequency-set --min 2.80GHz
+
+# turn on performance for all CPU
+for f in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
+  echo performance | sudo tee "$f"
+done
+
+# disable turbo as it is less stable
+echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+
+# run benchmark
+hyperfine --warmup 7 --runs 25 "taskset -c 2 ./run" --export-markdown data/benchmark.md
+
+# disable cpu limit
+sudo cpupower frequency-set -g performance
